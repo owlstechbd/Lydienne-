09bc6d76805d9368a658b317f35502f4800f8044
owlstechbd <owlstech.net@gmail.com>
2023-08-13 19:02:42 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  var owl = $('.owl-carousel');
+  owl.owlCarousel({
+    loop: true,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      }
+    }
+  });
+});
+
+
+// animation
+
+  new WOW().init();
